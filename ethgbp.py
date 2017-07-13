c25855acf3854d0a458ad5587fccdf6640a4221b
Mourad <mourad@ECS0546.local>
2017-07-13 15:09:28 +0100

    refactored in 2 funcs

--- ethgbp.py
+++ ethgbp.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python
 import requests, json, redis
 
-rdb=redis.StrictRedis(host="localhost", port=6379, db=0)
-#url = "https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=GBP"
-url = "https://api.cryptonator.com/api/full/eth-gbp"
-Resp = requests.get(url)
+rdb = ""
+def initRedis():
+    rdb=redis.StrictRedis(host="localhost", port=6379, db=0)
+    #url = "https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=GBP"
+    print rdb.keys()
+    return rdb
 
-# For successful API call, response code will be 200 (OK)
-if(Resp.ok):
-    ticker = json.loads(Resp.content)
-    print "timestamp: ",ticker["timestamp"]
-    timestamp = ticker["timestamp"]
-    print "ETH/GBP: ",ticker["ticker"]["price"]
-    price = ticker["ticker"]["price"]
-    rdb.set(timestamp,price)
-else:
-    Resp.raise_for_status()
+def getETHRate(rdb):
+    url = "https://api.cryptonator.com/api/full/eth-gbp"
+    Resp = requests.get(url)
 
-print rdb.keys()
+    # For successful API call, response code will be 200 (OK)
+    if(Resp.ok):
+        ticker = json.loads(Resp.content)
+        print "timestamp: ",ticker["timestamp"]
+        timestamp = ticker["timestamp"]
+        print "ETH/GBP: ",ticker["ticker"]["price"]
+        price = ticker["ticker"]["price"]
+        rdb.set(timestamp,price)
+    else:
+        Resp.raise_for_status()
+
+####Main
+rdb=initRedis()
+getETHRate(rdb)
